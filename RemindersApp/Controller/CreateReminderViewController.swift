32cb8cecbcb7d0de1ec5594944b6bc4b7e267e78
Jose Carlos <josealarconchacon@gmail.com>
2021-02-10 23:03:46 -0500

    add CreateReminderViewController file

--- RemindersApp/Controller/CreateReminderViewController.swift
+++ RemindersApp/Controller/CreateReminderViewController.swift
@@ -5,4 +5,30 @@
 //  Created by Jose Alarcon Chacon on 2/10/21.
 //
 
-import Foundation
+import UIKit
+
+class CreateReminderViewController: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var setDateLabel: UILabel!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var completedLabel: UILabel!
+    @IBOutlet weak var completedSwitch: UISwitch!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func comleted(_ sender: UISwitch) {
+    }
+    
+    @IBAction func saveButton(_ sender: UIButton) {
+        // create a reminder object
+        let reminder = Reminder(title: textField.text!, date: datePicker.date, isCompleted: completedSwitch.isOn)
+        ReminderService.share.createReminder(reminder: reminder)
+        // dissmis controller
+        navigationController!.popViewController(animated: true)
+    }
+}
