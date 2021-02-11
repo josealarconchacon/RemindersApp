afe82003c25b570fc4aeb000880fc4b9e49d8fea
Jose Carlos <josealarconchacon@gmail.com>
2021-02-10 20:47:07 -0500

    create a favorite method

--- RemindersApp/Controller/FavoriteReminderViewController.swift
+++ RemindersApp/Controller/FavoriteReminderViewController.swift
@@ -12,6 +12,18 @@ class FavoriteReminderViewController: UIViewController {
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var titleLabel: UILabel!
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        favorite()
+    }
     
-    
+    func favorite() {
+        if let favoriteReminder = ReminderService.share.favoriteReminder() {
+            titleLabel.text = favoriteReminder.title
+            // date
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "MM/dd/yy hh:mma"
+            dateLabel.text = dateFormatter.string(from: favoriteReminder.date)
+        }
+    }
 }
