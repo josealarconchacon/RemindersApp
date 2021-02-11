e9a0c6be419f85ae4ab8a0edebb672e5132168e9
Jose Carlos <josealarconchacon@gmail.com>
2021-02-10 19:05:49 -0500

    Setup Favorite VC

---
+++ RemindersApp/Controller/FavoriteReminderViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteReminderViewController.swift
+//  RemindersApp
+//
+//  Created by Jose Alarcon Chacon on 2/10/21.
+//
+
+import UIKit
+
+class FavoriteReminderViewController: UIViewController {
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    
+}
