0db3616b6d45cd8446a176b2753f38f6f33aa716
Jose Carlos <josealarconchacon@gmail.com>
2021-02-10 19:02:13 -0500

    Setup RemindersViewController

--- RemindersApp/Controller/RemindersViewController.swift
+++ RemindersApp/Controller/RemindersViewController.swift
@@ -5,4 +5,32 @@
 //  Created by Jose Alarcon Chacon on 2/10/21.
 //
 
-import Foundation
+import UIKit
+
+class RemindersViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+extension RemindersViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ReminderService.share.getNumberOfReminder()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Deque the cell from reuse pool
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ReminderCell") as! ReminderCell
+        // Get the reminder for the given index path
+        let reminder = ReminderService.share.getReminder(index: indexPath.row)
+        // Update the cell
+        cell.update(reminder: reminder)
+        // Return cell
+        return cell
+    }
+}
