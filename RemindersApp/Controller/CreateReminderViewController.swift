fad9272ba6606fe19028c83c695f67c0c928f7bc
Jose Carlos <josealarconchacon@gmail.com>
2021-02-11 22:51:38 -0500

    remove duplicate line of

--- RemindersApp/Controller/CreateReminderViewController.swift
+++ RemindersApp/Controller/CreateReminderViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class CreateReminderViewController: UIViewController {
     
+    var reminderIndex: Int?
+    
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var setDateLabel: UILabel!
@@ -19,15 +21,36 @@ class CreateReminderViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        getReminde()
+        if #available(iOS 14, *) {
+            datePicker.preferredDatePickerStyle = .inline
+        }
     }
     
-    @IBAction func comleted(_ sender: UISwitch) {
+    // if a reminder exists, populate it on the screen
+    func getReminde() {
+        if let reminderIndex = reminderIndex {
+            let reminder = ReminderService.share.getReminder(index: reminderIndex)
+            textField.text = reminder.title
+            datePicker.date = reminder.date
+            completedSwitch.isOn = reminder.isCompleted
+        }
     }
+    
+ 
     
     @IBAction func saveButton(_ sender: UIButton) {
         // create a reminder object
         let reminder = Reminder(title: textField.text!, date: datePicker.date, isCompleted: completedSwitch.isOn)
-        ReminderService.share.createReminder(reminder: reminder)
+        
+        
+        // update save Button so can tell the difference between new or updating an existing reminder
+        // update reminder
+        if let reminderIndex = reminderIndex {
+            ReminderService.share.updateReminder(reminder: reminder, index: reminderIndex)
+        } else { // create a reminder
+            ReminderService.share.createReminder(reminder: reminder)
+        }
         // dissmis controller
         navigationController!.popViewController(animated: true)
     }
